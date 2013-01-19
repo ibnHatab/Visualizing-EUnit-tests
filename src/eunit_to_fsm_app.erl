e0ce427458232cbc73e85c699fd12970ec21575f
VKI <lib.aca55a@gmail.com>
2013-01-19 13:33:42 +0200

    add application fake to be uses with rebar

---
+++ src/eunit_to_fsm_app.erl
@@ -0,0 +1,16 @@
+-module(eunit_to_fsm_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    eunit_to_fsm_sup:start_link().
+
+stop(_State) ->
+    ok.
